02472747a4e0c64d3f640f3efeda92663a05cb74
Toporkov Igor <iigortoporkov@gmail.com>
2019-02-24 16:54:09 +0300

    Shortened and renamed command

--- resources/input/command.sh
+++ resources/input/command.sh
@@ -1,1 +1,1 @@
-ffmpeg -f lavfi -i nullsrc=s=1280x720 -filter_complex "geq=random(1)*255:128:128;aevalsrc=-2+random(0)" -t 30 white_noise.mkv
+ffmpeg -f lavfi -i nullsrc=s=1280x720 -filter_complex "geq=random(1)*255:128:128;aevalsrc=-2+random(0)" -t 15 white_noise_h264.mkv
